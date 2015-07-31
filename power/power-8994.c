a1184f5de74741b6b9d97d3bdaa2183da81110b4
Steve Kondik <steve@cyngn.com>
2015-07-31 08:31:10 -0700

    power: More tunings
    Change-Id: I030f169b76fe88f4d99894e1f2ce5478947ad87c

--- power/power-8994.c
+++ power/power-8994.c
@@ -184,6 +184,7 @@ int power_hint_override(__attribute__((unused)) struct power_module *module,
         return HINT_HANDLED;
     }
 
+<<<<<<< HEAD
     if (hint == POWER_HINT_LAUNCH_BOOST) {
         int duration = 2000;
         int resources[] = { SCHED_BOOST_ON, 0x20D };
@@ -196,6 +197,11 @@ int power_hint_override(__attribute__((unused)) struct power_module *module,
     if (hint == POWER_HINT_CPU_BOOST) {
         int duration = (hintdata)data / 1000;
         int resources[] = { SCHED_BOOST_ON };
+=======
+    if (hint == POWER_HINT_CPU_BOOST) {
+        int duration = (hintdata)data / 1000;
+        int resources[] = { CPUS_ONLINE_MIN_2, SCHED_BOOST_ON, 0x20B, 0x30B };
+>>>>>>> power: More tunings
 
         if (duration > 0)
             interaction(duration, sizeof(resources)/sizeof(resources[0]), resources);
